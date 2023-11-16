a8ebcf8b06cff8e382fcd0998bb459ea8837fc3e
Jesús Germade <jesus@germade.dev>
2023-11-16 18:53:28 +0000

    Adding chaining tests

--- src/state.test.js
+++ src/state.test.js
@@ -10,13 +10,13 @@ describe('StateProxy', () => {
         expect(stateProxy).toBeInstanceOf(StateProxy)
     })
 
-    test('createState({ foo: "bar" })', () => {
+    test('new StateProxy({ foo: "bar" })', () => {
         const { state } = new StateProxy({ foo: 'bar' })
 
         expect(state.foo).toBe('bar')
     })
 
-    test('createState.onChange("foo")', () => {
+    test('new StateProxy.onChange("foo")', () => {
         const { state, onChange } = new StateProxy({ foo: 'foo' })
         const listener = vi.fn()
 
@@ -27,7 +27,7 @@ describe('StateProxy', () => {
         expect(listener).toBeCalled()
     })
 
-    test('createState.onChange("foo.bar"): 0', () => {
+    test('new StateProxy.onChange("foo.bar"): 0', () => {
         const { onChange } = new StateProxy({ foo: { bar: 'bar' } })
         const listener = vi.fn()
 
@@ -36,7 +36,7 @@ describe('StateProxy', () => {
         expect(listener).not.toBeCalled()
     })
 
-    test('createState.onChange("foo.bar"): 1', () => {
+    test('new StateProxy.onChange("foo.bar"): 1', () => {
         const { state, onChange } = new StateProxy({ foo: 'foo' })
         const listener = vi.fn()
 
@@ -47,7 +47,7 @@ describe('StateProxy', () => {
         expect(listener).toBeCalledTimes(1)
     })
 
-    test('createState.onChange("foo.bar"): 2', () => {
+    test('new StateProxy.onChange("foo.bar"): 2', () => {
         const { state, onChange } = new StateProxy({ foo: 'foo' })
         const listener = vi.fn()
 
@@ -65,7 +65,7 @@ describe('StateProxy', () => {
         expect(listener).toBeCalledWith('baz', 'bar')
     })
 
-    test('createState.onChange("foo.bar"): []', () => {
+    test('new StateProxy.onChange("foo.bar"): []', () => {
         const { state, onChange } = new StateProxy({ foo: [] })
         const listener = vi.fn()
         const listenerLength = vi.fn()
@@ -87,7 +87,7 @@ describe('StateProxy', () => {
         expect(listenerLength).toBeCalledWith(2)
     })
 
-    test('createState.onAnyChange', () => {
+    test('new StateProxy.onAnyChange', () => {
         const { state, onAnyChange } = new StateProxy({ foo: 'foo' })
         const listener = vi.fn()
 
@@ -97,5 +97,33 @@ describe('StateProxy', () => {
 
         expect(listener).toBeCalledTimes(1)
         expect(listener).toBeCalledWith('foo', 'bar', 'foo')
+    })
+
+    test('new StateProxy chaning onChange', () => {
+        const listener = vi.fn()
+
+        const { state } = new StateProxy({ foo: 'foo' })
+            .onChange('foo', listener)
+
+        state.foo = 'bar'
+
+        expect(listener).toBeCalledTimes(1)
+    })
+
+    test('new StateProxy chaning onChange 2 times', () => {
+        const listenerFoo = vi.fn()
+        const listenerBar = vi.fn()
+
+        const { state } = new StateProxy({ foo: 'foo' })
+            .onChange('foo', listenerFoo)
+            .onChange('bar', listenerBar)
+
+        state.foo = 'bar'
+
+        expect(listenerFoo).toBeCalledTimes(1)
+
+        state.bar = 'baz'
+
+        expect(listenerBar).toBeCalledTimes(1)
     })
 })
